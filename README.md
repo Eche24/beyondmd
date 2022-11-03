3b83885e1e434fa8f41e8b709fa06c8d606bda3b
eche24 <spwesn@gmail.com>
2022-11-03 23:50:10 +0000

    add all files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# beyondmd
-Assignment
+# Beyondmd Test
+
+Here are the prompts:
+
+Make a simple page using HTML that displays “Hello BeyondMD!“, and then dockerize it. It must run using docker-compose.
+
+Make a basic shell script for Debian that reads out system information including cpu temperature, memory usage, and the number of packages that have an upgrade availabe
+
+
---
+++ scripts/system_information.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Author: Eche Ngbede
+# Description: This script will list system information, CPU temperature, Memory usage, and upgradable package.
+# Date: 11/03/2022
+# Modified: 
+
+
+echo "Hostname:"
+hostname
+echo "-------------------------------"
+echo "Date:"
+date
+echo "-------------------------------"
+
+echo "IP address:"
+hostname -I
+echo "-------------------------------"
+
+echo "Uptime:"
+uptime
+echo "-------------------------------"
+
+echo "Last logins:"
+last -a | head -3
+echo "-------------------------------"
+
+echo "System information:"
+hostnamectl | grep  -iE "Kernel|operating"
+lscpu | head -n 19
+echo "-------------------------------"
+
+echo "Disk and memory usage:"
+free -h | grep m
+df -Th 
+echo "-------------------------------"
+
+echo "CPU temperature"
+
+echo "-------------------------------"
+
+echo "processes activity:"
+top -b | head -10 
+echo "-------------------------------"
+
+echo "List of upgradeable package:"
+apt list --upgradable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
