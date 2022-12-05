294366d0d6a24a7972ec9dddd1e18aa6349037d4
eche ngbede <spwesn@gmail.com>
2022-12-04 19:23:40 -0600

    Update Delete_files.sh

--- scripts/Delete_files.sh
+++ scripts/Delete_files.sh
@@ -1,5 +1,8 @@
-
-
+#!/bin/bash
+# Author: Eche Ngbede
+# Description: Remove files old then 7 days by creating a cronJobs to run every night
+# Date: 12/03/2022
+# Modified: 12/03/2022
 
 
 0 2 * * * /bin/find /var/log/<>/ -type f -mtime +7 -exec rm -rf {} \;
